5664ac8c42a018e54030301797be7a22f88908e0
BIJON SAHA <94054428+Bijon07@users.noreply.github.com>
2021-11-30 23:32:45 +0600

    Add files via upload

---
+++ UVA_13131.cpp
@@ -0,0 +1,50 @@
+///BIJON SAHA DURJOY
+//SWE_19
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+     long long int t;
+     std::cin >> t;
+     while (t--)
+     {
+          long long int n, k, i, x, y;
+          std::cin >> n >> k;
+          if (k == 1)
+          {
+               std::cout << 0 << endl;
+               continue;
+          }
+          long long sum = 0;
+
+          for (i = 1; i <= sqrt(n); i++)
+          {
+               if (n % i == 0)
+               {
+                    x = i;
+                    y = n / i;
+
+                    if (x == y)
+                    {
+                         if (x % k != 0)
+                         {
+                              sum = sum + x;
+                         }
+                    }
+                    else
+                    {
+                         if (x % k != 0)
+                         {
+                              sum = sum + x;
+                         }
+                         if (y % k != 0)
+                         {
+                              sum = sum + y;
+                         }
+                    }
+               }
+          }
+          std::cout << sum << endl;
+     }
+     return 0;
+}
