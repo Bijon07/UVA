17ddc32802c68c5dc3be3c505660bc84385a4d27
BIJON SAHA <94054428+Bijon07@users.noreply.github.com>
2021-12-01 11:25:17 +0600

    UVA  11044

---
+++ main.cpp
@@ -0,0 +1,20 @@
+///BIJON SAHA DURJOY
+// SUST_SWE_19
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+     int t;
+     std:: cin>> t;
+     while(t--)
+     {
+          int a, b;
+          std:: cin>>a>>b;
+          int rslt;
+
+          rslt = (a/3) * (b/3);
+          std:: cout<< rslt <<endl;
+     }
+
+     return 0;
+}
